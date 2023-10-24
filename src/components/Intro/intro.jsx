0f71f00325b7e5face21649660b0101967465642
imAbhaySingh <abhaybhadauriya2003@gmail.com>
2023-10-24 21:26:51 +0530

    First Commit Portfolio

---
+++ src/components/Intro/intro.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./intro.css";
+import bg from "../../assets/image.png";
+import {Link} from 'react-scroll';
+import btnImg from "../../assets/hireme.png"
+
+const Intro = () => {
+  return (
+    <section id="intro">
+      <div className="introContent">
+        <span className="hello">Hello,</span>
+        <span className="introText">
+          I'm
+          <span className="introName"> Abhay</span>
+          <br />
+          Web Developer
+        </span>
+        <p className="introPara">
+          I am a skilled wev developer with experience in   creating <br/> visually appealing and user friendly websites.
+        </p>
+        <Link><button className="btn"><img src={btnImg} alt="Hire Me" className="btnImg" />Hire Me</button></Link>
+      </div>
+      <img src={bg} alt="Profile" className="bg" />
+    </section>
+  );
+};
+
+export default Intro;
---
+++ src/components/Skills/skills.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './skills.css'
+import UIDeisgn from '../../assets/ui-design.png';
+import WebDesign from '../../assets/website-design.png';
+import AppDesign from '../../assets/app-design.png';
+
+const Skills = () => {
+  return (
+    <section id="skills">
+        <span className="skillsTitle">
+          What I do 
+          <span className="skillsDesc">
+            I am skilled and apssionate desginer with experience in creatinh visually appealing ans user friendly wevistes. I have a strong understanding of design and a keen eye for detail. I am proficient in HTML, CSS and React.js, as well as design software such as adobe photoshop and Illustrator.
+            <div className="skillsBars">
+              <div className="skillBar">
+                <img src={UIDeisgn} alt="" className="skillBarImg" />
+                <div className="skillBartext">
+                  <h2>UI/UX Design</h2>
+                  <p>Write some content here !</p>
+                </div>
+              </div>
+              <div className="skillBar">
+                <img src={WebDesign} alt="" className="skillBarImg" />
+                <div className="skillBartext">
+                <h2>Web Design</h2>
+                  <p>Write some content here !</p>
+                </div>
+              </div>
+              <div className="skillBar">
+                <img src={AppDesign} alt="" className="skillBarImg" />
+                <div className="skillBartext">
+                <h2>App Design</h2>
+                  <p>Write some content here !</p>
+                </div>
+              </div>
+            </div>
+          </span>
+        </span>
+    </section>
+  )
+}
+
+export default Skills
